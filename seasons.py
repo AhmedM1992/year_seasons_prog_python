e89ac6672922741ce377d99fec57f880a581c7f0
Ahmed Al-Kanan <32939040+AhmedM1992@users.noreply.github.com>
2017-12-01 11:44:58 +0200

    Update seasons.py

--- seasons.py
+++ seasons.py
@@ -1,15 +1,15 @@
 season = input("Enter month's name: ")
-lst_1 = ['January', 'February', 'December']
-lst_2 = ['March', 'April', 'May']
-lst_3 = ['June', 'July', 'August']
-lst_4 = ['September', 'October', 'November']
-if season in lst_1:
+winter = ['January', 'February', 'December']
+spring = ['March', 'April', 'May']
+summer = ['June', 'July', 'August']
+fall = ['September', 'October', 'November']
+if season in winter:
     print ("The season is Winter")
-elif season in lst_2:
+elif season in spring:
     print ("The season is Spring")
-elif season in lst_3:
+elif season in summer:
     print ("The season is Summer")
-elif season in lst_4:
+elif season in fall:
     print ("The season is Fall")
 else:
     print ("Go to sleep, BOY!")
