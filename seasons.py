5e141777114b29453d5dfe87834aa0bcf53d768a
Ahmed Al-Kanan <32939040+AhmedM1992@users.noreply.github.com>
2017-12-04 11:13:12 +0200

    Update seasons.py

--- seasons.py
+++ seasons.py
@@ -3,13 +3,14 @@ winter = ['January', 'February', 'December', 1, 2, 12]
 spring = ['March', 'April', 'May', 3, 4, 5]
 summer = ['June', 'July', 'August', 6, 7, 8]
 fall = ['September', 'October', 'November', 9, 10, 11]
-if season in winter:
-    print ("The season is Winter")
-elif season in spring:
-    print ("The season is Spring")
-elif season in summer:
-    print ("The season is Summer")
-elif season in fall:
-    print ("The season is Fall")
-else:
-    print ("Go to sleep, BOY!")
+def seasons():
+    if season in winter:
+        print ("The season is Winter")
+    elif season in spring:
+        print ("The season is Spring")
+    elif season in summer:
+        print ("The season is Summer")
+    elif season in fall:
+        print ("The season is Fall")
+    else:
+        print ("Go to sleep, BOY!")
