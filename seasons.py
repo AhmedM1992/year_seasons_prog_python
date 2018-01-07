5d3c8d5129bce34c1464bb4ff6bbc075214add77
Ahmed Al-Kanan <32939040+AhmedM1992@users.noreply.github.com>
2018-01-07 20:37:36 +0200

    Update seasons.py

--- seasons.py
+++ seasons.py
@@ -3,6 +3,8 @@ winter = ['January', 'February', 'December', 1, 2, 12]
 spring = ['March', 'April', 'May', 3, 4, 5]
 summer = ['June', 'July', 'August', 6, 7, 8]
 fall = ['September', 'October', 'November', 9, 10, 11]
+
+
 def seasons():
     if season in winter:
         print ("The season is Winter")
